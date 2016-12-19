b16896c9dac152f04d635f835c465f49a83f75e1
Robert Horrocks <rhorrocks@bds.local>
2016-12-19 14:50:46 -0500

    adding info on bool, integral, float and string data types

--- FromTheTopCSharpSolutionBuilder/Program.cs
+++ FromTheTopCSharpSolutionBuilder/Program.cs
@@ -41,6 +41,7 @@ namespace FromTheTopCSharpSolutionBuilder
             //of using a method call to print a new line. 
 
             SecondLine();
+            ThirdClass();
         }
 
         static void SecondLine()
@@ -71,6 +72,73 @@ namespace FromTheTopCSharpSolutionBuilder
 
             Console.ReadKey();
 
+        }
+
+        static void ThirdClass()
+        {
+            //C# has many different built-in types.
+
+            //Boolean (true or false)
+
+            bool firstBool = true;
+            bool secondBool = false;
+
+            Console.WriteLine("C# has a Boolean function, which allows users to set a {0} or {1} condition", 
+                firstBool, secondBool);
+            Console.WriteLine("Please press any key to continue.");
+
+            //Integral Data Types
+            //Go to MSDN (Microsoft Developer Network) to get value ranges, 
+            //or let Visual Studio figure it for you
+            //sbyte (signed byte)   
+            //byte                  
+            //short         
+            //ushort
+            //int
+            //uint
+            //long
+            //ulong
+
+            Console.WriteLine("C# offers many integral data types.");
+            Console.WriteLine("The minimum and maximum int values are {0}, {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("The minimum and maximum uint values are {0}, {1}", uint.MinValue, uint.MaxValue);
+            Console.WriteLine("The minimum and maximum sbyte values are {0}, {1}", sbyte.MinValue, sbyte.MaxValue);
+            Console.WriteLine("The minimum and maximum byte values are {0}, {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("The minimum and maximum short values are {0}, {1}", short.MinValue, short.MaxValue);
+            Console.WriteLine("The minimum and maximum ushort values are {0}, {1}", ushort.MinValue, ushort.MaxValue);
+            Console.WriteLine("The minimum and maximum long values are {0}, {1}", long.MinValue, long.MaxValue);
+            Console.WriteLine("The minimum and maximum ulong values are {0}, {1}", ulong.MinValue, ulong.MaxValue);
+            Console.WriteLine("Please press any key to continue.");
+            Console.ReadKey();
+
+            //Float Data Types
+            //float
+            //double
+            //decimal
+
+            Console.WriteLine("There are also a few types of decimal placed numbers.");
+            Console.WriteLine("The minimum and maximum float values are {0}, {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("The minimum and maximum double values are {0}, {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine("The minimum and maximum decimal values are {0}, {1}", decimal.MinValue, decimal.MaxValue);
+            Console.WriteLine("Please press any key to continue.");
+            Console.ReadKey();
+
+            //Strings and Escape Characters
+            //Escape sequence/character is the \ backslash key
+            //There are some non-printable characters
+            //The entire list of escape sequences are available on MSDN at https://msdn.microsoft.com/en-us/library/h21280bw.aspx.
+
+            Console.WriteLine("There are a number of characters that must be read with an escape character.\nFor example, the double quotes\"\" must be escaped to be seen on a screen."); 
+            string word = "\"Word\"";
+            Console.WriteLine(word);
+
+            //C# also has a Verbatim Literal way to literally take items verbatim. 
+            Console.WriteLine("You can also use the verbatim literal @ in a string to show the complete, as-typed string");
+            string verbatimLiteral = @"C:\Programs\VisualStudio";
+            Console.WriteLine("For example: " + verbatimLiteral);
+            Console.WriteLine("Please press any key to continue.");
+            Console.ReadKey();
+            
         }
 
             
